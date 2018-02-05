c58d14f5d1f698b5d0fe4a22b90a472b2d797146
ninna.boethius <ninna.boethius@leanovate.de>
2018-02-05 16:21:24 +0100

    folderify

---
+++ main/java/com/learn/graphql/bareminimum/BareminimumApplication.java
@@ -0,0 +1,12 @@
+package com.learn.graphql.bareminimum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BareminimumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BareminimumApplication.class, args);
+	}
+}

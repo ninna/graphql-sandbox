5cbbef2e11db3cb2df643b36cf3323411e2ade59
Markus Undhagen <markus.undhagen@gmail.com>
2018-02-17 09:35:41 +0100

    do tutorial

--- main/java/com/learn/graphql/bareminimum/BareminimumApplication.java
+++ main/java/com/learn/graphql/bareminimum/BareminimumApplication.java
@@ -1,12 +1,75 @@
 package com.learn.graphql.bareminimum;
 
+import com.learn.graphql.bareminimum.model.Author;
+import com.learn.graphql.bareminimum.model.Book;
+import com.learn.graphql.bareminimum.model.error.GraphQLErrorAdapter;
+import com.learn.graphql.bareminimum.model.repository.AuthorRepository;
+import com.learn.graphql.bareminimum.model.repository.BookRepository;
+import com.learn.graphql.bareminimum.model.resolver.BookResolver;
+import com.learn.graphql.bareminimum.model.resolver.Mutation;
+import com.learn.graphql.bareminimum.model.resolver.Query;
+import graphql.ExceptionWhileDataFetching;
+import graphql.GraphQLError;
+import graphql.servlet.GraphQLErrorHandler;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class BareminimumApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BareminimumApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BareminimumApplication.class, args);
+    }
+
+    @Bean public BookResolver authorResolver(AuthorRepository authorRepository) {
+        return new BookResolver(authorRepository);
+    }
+
+    @Bean public Query query(AuthorRepository authorRepository, BookRepository bookRepository) {
+        return new Query(authorRepository, bookRepository);
+    }
+
+    @Bean public Mutation mutation(AuthorRepository authorRepository, BookRepository bookRepository) {
+        return new Mutation(authorRepository, bookRepository);
+    }
+
+    @Bean public CommandLineRunner demo(AuthorRepository authorRepository, BookRepository bookRepository) {
+        return (args) -> {
+            Author author = new Author("Herbert", "Schildt");
+            Book book = new Book("Java: A Beginner's Guide, Sixth Edition", "0071809252", 728, author);
+            authorRepository.save(author);
+            bookRepository.save(book);
+        };
+    }
+    @Bean
+    public GraphQLErrorHandler errorHandler() {
+        return new GraphQLErrorHandler() {
+
+            @Override
+            public List<GraphQLError> processErrors(List<GraphQLError> errors) {
+                List<GraphQLError> clientErrors = errors.stream()
+                        .filter(this::isClientError)
+                        .collect(Collectors.toList());
+                List<GraphQLError> serverErrors = errors.stream()
+                        .filter(e -> !isClientError(e))
+                        .map(GraphQLErrorAdapter::new)
+                        .collect(Collectors.toList());
+                List<GraphQLError> e = new ArrayList<>();
+                e.addAll(clientErrors);
+                e.addAll(serverErrors);
+                return e;
+            }
+
+            protected boolean isClientError(GraphQLError error) {
+                return !(error instanceof ExceptionWhileDataFetching || error instanceof Throwable);
+            }
+        };
+
+    }
 }
--- main/java/com/learn/graphql/bareminimum/model/resolver/Mutation.java
+++ main/java/com/learn/graphql/bareminimum/model/resolver/Mutation.java
@@ -1,6 +1,9 @@
 package com.learn.graphql.bareminimum.model.resolver;
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.learn.graphql.bareminimum.model.Author;
+import com.learn.graphql.bareminimum.model.Book;
+import com.learn.graphql.bareminimum.model.error.BookNotFoundException;
 import com.learn.graphql.bareminimum.model.repository.AuthorRepository;
 import com.learn.graphql.bareminimum.model.repository.BookRepository;
 
@@ -12,5 +15,39 @@ public class Mutation implements GraphQLMutationResolver {
     public Mutation(AuthorRepository authorRepository, BookRepository bookRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+    }
+
+    public Author newAuthor(String firstName, String lastName) {
+        Author author = new Author();
+        author.setFirstName(firstName);
+        author.setLastName(lastName);
+        authorRepository.save(author);
+        return author;
+    }
+
+    public Book newBook(String title, String isbn, Integer pageCount, Long authorId) {
+        Book book = new Book();
+        book.setAuthor(new Author(authorId));
+        book.setIsbn(isbn);
+        book.setPageCount(pageCount != null ? 0 : pageCount);
+        book.setTitle(title);
+
+        bookRepository.save(book);
+        return book;
+    }
+
+    public boolean deleteBook(Long bookId) {
+        bookRepository.delete(bookId);
+        return true;
+    }
+
+    public Book updateBookPageCount(Integer pageCount, Long id) {
+        Book book = bookRepository.findOne(id);
+        if (null == book) {
+            throw new BookNotFoundException("Could not find book to update", id);
+        }
+        book.setPageCount(pageCount);
+        bookRepository.save(book);
+        return book;
     }
 }
